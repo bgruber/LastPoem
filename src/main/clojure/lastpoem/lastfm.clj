61fcef4ac86138528177c54f18360c0d61108bca
bgruber <github.com@brian.iheardata.com>
2010-08-31 00:36:19 -0400

    remove :use from lastfm code

--- src/main/clojure/lastpoem/lastfm.clj
+++ src/main/clojure/lastpoem/lastfm.clj
@@ -1,8 +1,9 @@
 (ns lastpoem.lastfm
-  (:use clojure.xml
-	clojure.contrib.zip-filter.xml)
-  (:require [clojure.zip :as zip]
-	    [clojure-http.resourcefully :as resourcefully])
+  (:require [lastpoem.util :as util]
+			[clojure.xml :as xml]
+			[clojure.zip :as zip]
+			[clojure-http.resourcefully :as resourcefully]
+			[clojure.contrib.zip-filter.xml :as zip-xml])
   (:gen-class :main false))
 
 (def lastfm-base-url "http://ws.audioscrobbler.com/2.0/")
@@ -10,9 +11,12 @@
 
 (def initial-number-of-tracks 15)
 (defn recent-tracks [username]
-  (let [tracks (zip/xml-zip (parse (rest-url lastfm-base-url {"method" "user.getrecenttracks"
-							      "user" username
-							      "limit" initial-number-of-tracks
-							      "api_key" lastfm-api-key})))]
-    (zipmap (xml-> tracks :recenttracks :track :artist text)
-	    (xml-> tracks :recenttracks :track :name text))))
+  (let [tracks (zip/xml-zip
+				(xml/parse
+				 (util/rest-url lastfm-base-url
+								{"method" "user.getrecenttracks"
+								 "user" username
+								 "limit" initial-number-of-tracks
+								 "api_key" lastfm-api-key})))]
+    (zipmap (zip-xml/xml-> tracks :recenttracks :track :artist zip-xml/text)
+			(zip-xml/xml-> tracks :recenttracks :track :name zip-xml/text))))
