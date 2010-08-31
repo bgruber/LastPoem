59cf1c33ae9c44ff43a15d6ccb978f146b897e85
bgruber <github.com@brian.iheardata.com>
2010-08-30 23:19:13 -0400

    move poetry code to its own file

--- src/main/clojure/lastpoem/lastfm.clj
+++ src/main/clojure/lastpoem/lastfm.clj
@@ -25,17 +25,3 @@
 							      "api_key" lastfm-api-key})))]
     (zipmap (xml-> tracks :recenttracks :track :artist text)
 	    (xml-> tracks :recenttracks :track :name text))))
-
-(defn make-poetry [username]
-  (let [lyriclist (remove #(= '("Not found") %)
-			  (map #(apply fetch-lyrics %) (recent-tracks username)))
-	successful (count lyriclist)
-	nth-or-not (fn [n available]
-		     (if (= n (dec successful))
-		       (dec available)
-		       (if (< n (dec available))
-			 n
-			 (mod (- available (- successful n)) available))))]
-    (map (fn [n lyrics]
-	   (nth lyrics (nth-or-not n (count lyrics))))
-	 (range successful) lyriclist)))
---
+++ src/main/clojure/lastpoem/poetry.clj
@@ -0,0 +1,15 @@
+(ns lastpoem.poetry)
+
+(defn make-poetry [username]
+  (let [lyriclist (remove #(= '("Not found") %)
+						  (map #(apply fetch-lyrics %) (recent-tracks username)))
+		successful (count lyriclist)
+		nth-or-not (fn [n available]
+					 (if (= n (dec successful))
+					   (dec available)
+					   (if (< n (dec available))
+						 n
+						 (mod (- available (- successful n)) available))))]
+    (map (fn [n lyrics]
+		   (nth lyrics (nth-or-not n (count lyrics))))
+		 (range successful) lyriclist)))
