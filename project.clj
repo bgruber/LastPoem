6946492bfb263de897f2a6479135aba32363f5b4
bgruber <github.com@brian.iheardata.com>
2010-09-30 12:26:22 -0400

    Move to leiningen

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject lastpoem "0.0.1"
+  :description "The Last Poem"
+  :namespaces [lastpoem.app_servlet]
+  :dependencies [[org.clojure/clojure "1.2.0"]
+		 [org.clojure/clojure-contrib "1.2.0"]
+		 [compojure "0.5.1"]
+		 [enlive "1.0.0-SNAPSHOT"]]
+  :dev-dependencies [[appengine-magic "0.1.3"]
+		     [swank-clojure "1.2.1"]])
