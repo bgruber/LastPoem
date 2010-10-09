b293c54140240230244c82292c5f98763342fa8d
bgruber <github.com@brian.iheardata.com>
2010-10-09 17:30:53 -0400

    adding rudimentary routes to get poem

--- src/lastpoem/core.clj
+++ src/lastpoem/core.clj
@@ -1,8 +1,15 @@
 (ns lastpoem.core
   (:use [compojure.core])
-  (:require [appengine-magic.core :as ae]))
+  (:require [appengine-magic.core :as ae]
+            [clojure.pprint]
+            [compojure.route :as route]
+            [lastpoem.poetry]))
+
+(def pprint #(clojure.pprint/write % :stream nil))
 
 (defroutes lastpoem-app-handler
-  (GET "/" [] "Hello, world!"))
+  (GET "/poetry" []
+       (pprint (lastpoem.poetry/make-poetry "bgruber")))
+  (route/not-found "404"))
 
 (ae/def-appengine-app lastpoem-app #'lastpoem-app-handler)
